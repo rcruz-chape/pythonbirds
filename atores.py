f1a3513f332bec8e10fe2303ed35ab686241f1dd
rcruz-chape <renato.cruz.souza@hotmail.com>
2020-04-03 15:05:33 -0300

    Lançamento Vertical

--- atores.py
+++ atores.py
@@ -123,7 +123,10 @@ class Passaro(Ator):
         :param tempo: tempo de jogo a ser calculada a posição
         :return: posição x, y
         """
-        return 1,1
+        if self.foi_lancado():
+            delta_t = tempo-self._tempo_de_lancamento
+            self._calcular_posicao_vertical(delta_t)
+        return super().calcular_posicao(tempo)
 
 
 
@@ -138,6 +141,14 @@ class Passaro(Ator):
         """
         self._angulo_de_lancamento = angulo
         self._tempo_de_lancamento = tempo_de_lancamento
+
+    def _calcular_posicao_vertical(self, delta_t):
+        y_atual=self._y_inicial
+        angulo_radiano = math.radians(self._angulo_de_lancamento)
+        y_atual += self.velocidade_escalar*delta_t * math.sin(angulo_radiano)
+        y_atual -= (GRAVIDADE*(delta_t**2))/2
+        self.y=y_atual
+
 
 
 class PassaroAmarelo(Passaro):
